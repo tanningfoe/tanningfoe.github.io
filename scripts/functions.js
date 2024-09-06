d36de34f3ce510609f042bafee0d41d50c21fb05
tanningfoe <31169498+tanningfoe@users.noreply.github.com>
2024-09-06 16:34:07 +0000

    Add files via upload

--- scripts/functions.js
+++ scripts/functions.js
@@ -9,7 +9,11 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
-
+//Fucntion to hide welcome message
+function hideWelcomeMsg() {
+    const WelcomeMsg = document.querySelector('.overlay-box');
+    WelcomeMsg.style.display = 'none';
+}
 // Add event listeners to the buttons
 document.addEventListener('DOMContentLoaded', () => {
     const showButton = document.getElementById('showSidebarBtn');
