140c063e4029b57d1e588053cd8240153f8ef190
tanningfoe <31169498+tanningfoe@users.noreply.github.com>
2024-09-07 23:57:55 +0000

    Add files via upload

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,28 +1,36 @@
-// Function to show the sidebar
-function showSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const bottombanner = document.querySelector('.bottom-banner');
-    bottombanner.style.display = 'none';
-    sidebar.style.display = 'flex';
+//Function to hide bottom banner
+function hideBottonBanner() {
+    const welcomemsg = document.querySelector('.bottom-banner');
+    welcomemsg.style.visibility = 'hidden';
+}
+//Function to show bottom banner
+function showBottomBanner() {
+    const welcomemsg = document.querySelector('.bottom-banner');
+    welcomemsg.style.visibility = 'visible';
 }
 
-// Function to hide the sidebar
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const bottombanner = document.querySelector('.bottom-banner');
-    bottombanner.style.display = 'flex';
-    sidebar.style.display = 'none';
-}
 //Function to hide welcome message
 function hideWelcomeMsg() {
     const welcomemsg = document.querySelector('.overlay-box');
     welcomemsg.style.display = 'none';
 }
-// Add event listeners to the buttons
-document.addEventListener('DOMContentLoaded', () => {
-    const showButton = document.getElementById('showSidebarBtn');
-    const hideButton = document.getElementById('hideSidebarBtn');
+//Function to show welcome message
+function showWelcomeMsg() {
+    const welcomemsg = document.querySelector('.overlay-box');
+    welcomemsg.style.display = 'flex';
+}
 
-    showButton.addEventListener('click', showSidebar);
-    hideButton.addEventListener('click', hideSidebar);
-});
+// Function to show the sidebar
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    hideWelcomeMsg();
+    hideBottonBanner();
+    sidebar.style.display = 'flex';
+}
+// Function to hide the sidebar
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+    showWelcomeMsg();
+    showBottomBanner();
+}
