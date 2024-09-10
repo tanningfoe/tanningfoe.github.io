d9151d2f2fda12c22ca279e8b6b9454d6513cda6
tanningfoe <31169498+tanningfoe@users.noreply.github.com>
2024-09-10 00:24:38 +0000

    Add files via upload

--- scripts/functions.js
+++ scripts/functions.js
@@ -20,16 +20,53 @@ function showWelcomeMsg() {
     welcomemsg.style.display = 'flex';
 }
 
+//Function to hide skills box
+function hideSkillsBox() {
+    const skillsbox = document.querySelector('.skills-box');
+    skillsbox.style.display = 'none';
+}
+//Function to show skills box
+function showSkillsBox() {
+    const skillsbox = document.querySelector('.skills-box');
+    skillsbox.style.display = 'flex';
+}
+
+//Function to hide about-me box
+function hideAboutMeBox() {
+    const aboutbox = document.querySelector('.about-me-box');
+    aboutbox.style.display = 'none';
+}
+//Function to show about-me box
+function showAboutMeBox() {
+    const aboutbox = document.querySelector('.about-me-box');
+    aboutbox.style.display = 'flex';
+}   
 // Function to show the sidebar
 function showSidebar() {
-    hideWelcomeMsg();
+    const welcomemsg = document.querySelector('.welcome-msg');
+    const skillsmsg  = document.querySelector('.skills-box');
+    const aboutbox  = document.querySelector('.about-me-box');
+    if (welcomemsg != null)
+        hideWelcomeMsg();
+    if (skillsmsg != null)
+        hideSkillsBox();
+    if (aboutbox != null)
+        hideAboutMeBox();
     hideBottonBanner();
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
 // Function to hide the sidebar
 function hideSidebar() {
-    showWelcomeMsg();
+    const welcomemsg = document.querySelector('.welcome-msg');
+    const skillsbox  = document.querySelector('.skills-box');
+    const aboutbox  = document.querySelector('.about-me-box');
+    if (welcomemsg != null)
+        showWelcomeMsg();
+    if (skillsbox != null)
+        showSkillsBox();
+    if (aboutbox != null)
+        showAboutMeBox();
     showBottomBanner();
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';   
